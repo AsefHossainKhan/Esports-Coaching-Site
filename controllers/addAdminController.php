32bf7ad20f7d51a8077a709c760e4c23ad52a7d8
Masudul Hasan Joarder <65823803+joarder97@users.noreply.github.com>
2020-09-19 21:22:16 +0600

    Add files via upload

---
+++ controllers/addAdminController.php
@@ -0,0 +1,33 @@
+<?php
+
+    require_once("../services/userServices.php");
+
+    $username = $_POST["username"];
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $confirmPassword = $_POST["confirmPassword"];
+
+    $statusUserName = isUsernameUnique($username);
+    $status = isEmailUnique($email);
+    $statusPassword = isPasswordMached($password, $confirmPassword);
+
+    if(empty($username) || empty($name) || empty($email) || empty($password) ||empty($confirmPassword)){
+        echo "Empty Fields Found";
+    }
+    else if($statusUserName == false){
+        echo "username not unique";
+    }
+    else if($status == false){
+        echo "email not unique";
+    }
+    else if($statusPassword == false){
+        echo "Password not mached";
+    }
+    else{
+        $userType = "Admin";
+        createUser($username,$name,$email,$password,$userType); 
+    }
+
+
+?>
