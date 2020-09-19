f3875b3a1013e9bb8def60ced25011bcdcf47f82
joarder97 <masudul.pallab99@gmail,com>
2020-09-19 20:47:51 +0600

    adminAdded

--- services/userServices.php
+++ services/userServices.php
@@ -119,26 +119,6 @@
     }
   }
 
-  function isPasswordMached($password, $confirmPassword){
-    if($password !== $confirmPassword){
-      return false;
-    }else{
-      return true;
-    }
-  }
-
-  function createUser($username, $name, $email, $password, $userType){
-    $connection = dbConnection(); 
-
-    $sql = "INSERT INTO users(userName,name,email,userType) VALUES('".$username."','".$name."','".$email."','".$userType."')";
-    $result = mysqli_query($connection, $sql);
-
-    $sql2 = "SELECT UserId FROM users WHERE userName='".$username."' and name='".$name."' and email='".$email."' LIMIT 1"; 
-    $result2 = mysqli_query($connection, $sql2);
-    $fetch = mysqli_fetch_assoc($result2);
-
-    $sql3 = "INSERT INTO login(userId,password) VALUES('".$fetch["UserId"]."','".$password."')";
-    mysqli_query($connection, $sql3);
-  }
+  
 
 ?>
