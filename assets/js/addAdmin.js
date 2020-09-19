47595eacad12d68ec6c7cc4901bbfa1bc5ff84f7
joarder97 <masudul.pallab99@gmail,com>
2020-09-19 17:17:05 +0600

    newAdminCreate

---
+++ assets/js/addAdmin.js
@@ -0,0 +1,38 @@
+function addAdminVisible(){
+    document.getElementById("addAdminForm").style.visibility = "visible";
+}
+
+function addAdmin(){
+    var username = document.getElementById("username").value;
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('POST', '../controllers/addAdminController.php', true);
+    xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhttp.send('username='+username+'&name='+name+'&email='+email+'&password='+password+'&confirmPassword='+confirmPassword);
+
+        xhttp.onreadystatechange = function(){
+                if(this.readyState == 4 && this.status == 200){
+                
+                if(this.responseText == "Empty Fields Found"){
+                    document.getElementById("error").innerHTML = "Empty Fields Found.";
+                }
+                else if(this.responseText == "username not unique"){
+                    document.getElementById("error").innerHTML = "Username Exists.";
+                }
+                else if(this.responseText == "email not unique"){
+                    document.getElementById("error").innerHTML = "Email is Not Unique.";
+                }
+                else if(this.responseText == "Password not mached"){
+                    document.getElementById("error").innerHTML = "Password Not Mached.";
+                }
+                else{
+                    document.getElementById("error").innerHTML = "User Created.";
+                }
+            }
+        }
+}
