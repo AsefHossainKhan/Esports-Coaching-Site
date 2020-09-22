7c304b9c309836547785fecc5111aa3fd4f318fb
Masudul Hasan Joarder <65823803+joarder97@users.noreply.github.com>
2020-09-23 04:01:06 +0600

    Add files via upload

---
+++ assets/js/showStudents.js
@@ -0,0 +1,22 @@
+function showStudents(){
+    document.getElementById("addAdminForm").style.visibility = "hidden";
+    document.getElementById("error").style.visibility = "hidden";
+    document.getElementById("showCoaches").style.visibility = "hidden";
+    document.getElementById("showAdmins").style.visibility = "hidden";
+    document.getElementById("coachApproval").style.visibility = "hidden";
+    document.getElementById("showStudents").style.visibility = "visible";
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('POST', '../controllers/showStudentsController.php', true);
+    xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhttp.send();
+
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            // document.getElementById("pendingApproval").innerHTML = "<table> border='1'";
+             document.getElementById("showStudents").innerHTML = this.responseText; 
+            //  document.getElementById("pendingApproval").innerHTML = "</table>";
+             //alert(this.responseText);
+        }
+    }
+}
