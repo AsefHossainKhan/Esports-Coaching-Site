62b4a924b6163ee950b65183dd1166f8b251947a
Masudul Hasan Joarder <65823803+joarder97@users.noreply.github.com>
2020-09-23 16:03:06 +0600

    Update addAdmin.js

--- assets/js/addAdmin.js
+++ assets/js/addAdmin.js
@@ -8,7 +8,7 @@ function addAdminVisible(){
 }
 
 function addAdmin(){
-    document.getElementById("error").style.visibility = "visible";
+   
 
     
 
@@ -17,31 +17,94 @@ function addAdmin(){
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var confirmPassword = document.getElementById("confirmPassword").value;
+    var question = document.getElementById("question").value;
+    var answer = document.getElementById("answer").value;
 
+    if(emailCheck() || passwordCheck() || password2Check()){
+        document.getElementById("error").style.visibility = "visible";
+        var xhttp = new XMLHttpRequest();
+        xhttp.open('POST', '../controllers/addAdminController.php', true);
+        xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhttp.send('username='+username+'&name='+name+'&email='+email+'&password='+password+'&confirmPassword='+confirmPassword+'&question='+question+'&answer='+answer);
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.open('POST', '../controllers/addAdminController.php', true);
-    xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhttp.send('username='+username+'&name='+name+'&email='+email+'&password='+password+'&confirmPassword='+confirmPassword);
-
-        xhttp.onreadystatechange = function(){
-                if(this.readyState == 4 && this.status == 200){
-                
-                if(this.responseText == "Empty Fields Found"){
-                    document.getElementById("error").innerHTML = "Empty Fields Found.";
+            xhttp.onreadystatechange = function(){
+                    if(this.readyState == 4 && this.status == 200){
+                    
+                    if(this.responseText == "Empty Fields Found"){
+                        document.getElementById("error").innerHTML = "Empty Fields Found.";
+                    }
+                    else if(this.responseText == "username not unique"){
+                        document.getElementById("error").innerHTML = "Username Exists.";
+                    }
+                    else if(this.responseText == "email not unique"){
+                        document.getElementById("error").innerHTML = "Email is Not Unique.";
+                    }
+                    else if(this.responseText == "Password not mached"){
+                        document.getElementById("error").innerHTML = "Password Not Mached.";
+                    }
+                    else if(this.responseText == "Please Enter security question and answer"){
+                        document.getElementById("error").innerHTML = "Please Enter security question and answer.";
+                    }
+                    else{
+                        document.getElementById("error").innerHTML = "User Created.";
+                    }
                 }
-                else if(this.responseText == "username not unique"){
-                    document.getElementById("error").innerHTML = "Username Exists.";
-                }
-                else if(this.responseText == "email not unique"){
-                    document.getElementById("error").innerHTML = "Email is Not Unique.";
-                }
-                else if(this.responseText == "Password not mached"){
-                    document.getElementById("error").innerHTML = "Password Not Mached.";
-                }
-                else{
-                    document.getElementById("error").innerHTML = "User Created.";
-                }
-            }
         }
+    }else{
+        document.getElementById("error").innerHTML = "Please fill valid informations.";
+    }
 }
+
+
+function emailCheck(){
+    document.getElementById("error").style.visibility = "visible";
+    const emailValue = email.value.trim();
+  if (emailValue === '') {
+        document.getElementById("error").innerHTML = "Email is Empty";
+        return false;
+  } 
+  else if (!emailValue.includes('@') || !emailValue.includes('.com') || emailValue.lastIndexOf('@') > emailValue.lastIndexOf('.com')) {
+        document.getElementById("error").innerHTML = "Not Valid Email";
+        return false;
+  }else{
+    document.getElementById("error").style.visibility = "hidden";
+        return true;
+  }
+}
+
+function passwordCheck() {
+    document.getElementById("error").style.visibility = "visible";
+    const passwordValue = password.value.trim();
+    if (passwordValue === '') {
+        document.getElementById("error").innerHTML = "Password Field is Empty.";
+      return false;
+    } 
+    else if (passwordValue.length < 8) {
+        document.getElementById("error").innerHTML = "Password must be a minimum of 8 characters";
+      return false;
+    }
+    else {
+        document.getElementById("error").style.visibility = "hidden";
+      return true;
+    }
+  
+  }
+
+  function password2Check() {
+    document.getElementById("error").style.visibility = "visible";
+    const password2Value = confirmPassword.value.trim();
+    const passwordValue = password.value.trim();
+    if (password2Value === '') {
+        document.getElementById("error").innerHTML = "Confirm Password Field is Empty.";
+      return false;
+    } 
+    else if (passwordValue != password2Value) {
+        document.getElementById("error").innerHTML = "Passwords Not Mached.";
+      return false;
+    }
+    else {
+      document.getElementById("error").style.visibility = "hidden";
+      return true;
+    }
+  
+  }
